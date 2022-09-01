779efec5f7e335cadb9977d94f60219bd263533f
Thembisile <tgwebeda@users.noreply.github.com>
2022-09-01 21:16:47 +0200

    Update Scroll.js

--- JavaScript/Scroll.js
+++ JavaScript/Scroll.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll',()=>{
     let content = document.querySelector('.rows');
     let contentPosition = content.getBoundingClientRect().top;
-    let screenPosition = window.innerHeight /1.1;
+    let screenPosition = window.innerHeight /.5;
         if(contentPosition < screenPosition){
         content.classList.add('active');
    }else{
